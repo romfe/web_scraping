0fb12543838ff4831eabc8812c8b827c6eff8aa3
Romulo <romfe89@gmail.com>
2021-02-28 16:45:15 -0300

    improvements to the python program

--- pysearch.py
+++ pysearch.py
@@ -1,10 +1,23 @@
 import requests
 import subprocess
 import optparse
+from bs4 import BeautifulSoup
 
 parser = optparse.OptionParser()
 parser.add_option("-u","--url", dest = "url", help = "url of the website to be analyzed")
-
+parser.add_option("-s", "--string", dest = "string", help = "string to search for on the page")
 (options, arguments) = parser.parse_args()
 url = options.url
+string = options.string
+
 print(url)
+print(string)
+
+try:
+    page = requests.get(url)
+except:
+    print("[-] Error accessing the website. Please verify the url and try again")
+    exit()
+
+rawHtml = BeautifulSoup(page.content, "html.parser")
+print(rawHtml)
